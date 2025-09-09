12274d75c7d47ddac6311e77c3ba6075b4e809f6
YiaGrisha <yiagrisha69@gmail.com>
2025-09-09 11:58:57 +0300

    UI and animation update

--- script.js
+++ script.js
@@ -8,8 +8,29 @@ gsap.registerPlugin(ScrollTrigger)
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
+if ('scrollRestoration' in history) {
+   history.scrollRestoration = 'manual'
+}
+
 const canvas = document.querySelector('canvas.webgl')
 document.body.style.overflow = 'hidden'
+
+/* 
+Script 
+*/
+      const scrollContainer = document.querySelector('#warmth .scroll-row');
+      const leftBtn = document.querySelector('#warmth .scroll-left');
+      const rightBtn = document.querySelector('#warmth .scroll-right');
+
+      if (scrollContainer && leftBtn && rightBtn) {
+      leftBtn.onclick = () => {
+         scrollContainer.scrollBy({left: -300, behavior: 'smooth'});
+      };
+      rightBtn.onclick = () => {
+         scrollContainer.scrollBy({left: 300, behavior: 'smooth'});
+      };
+      }
+         
 
 /* 
 Debug 
@@ -31,13 +52,22 @@ LOADING
    let introDone = false
    const objectsDistance = 3
 
-   window.addEventListener('load', function() {
-      this.setTimeout(function() { window.scrollTo(0, 0), 0 })
-      
-      const overlay = this.document.querySelector('.overlay')
-      const title = this.document.querySelector('.title')
+   if ('scrollRestoration' in history) {
+      history.scrollRestoration = 'manual'
+   }
 
-      setTimeout(function() {
+   function resetScroll() {
+      setTimeout(() => { window.scrollTo(0, 0) }, 100) 
+   }
+
+   window.addEventListener('DOMContentLoaded', resetScroll)
+   window.addEventListener('load', resetScroll)
+
+   window.addEventListener('load', function() {
+      const overlay = document.querySelector('.overlay')
+      const title = document.querySelector('.title')
+
+      setTimeout(() => {
          overlay.classList.add('hidden')
          title.classList.add('visible')
       }, 1000)
@@ -56,6 +86,8 @@ Resize
       height: window.innerHeight
    }
 
+   const baseHeight = 900
+
    window.addEventListener('resize', function(event) {
       // Update sizes
       sizes.width = window.innerWidth
@@ -68,6 +100,13 @@ Resize
       // Update renderer
       renderer.setSize(sizes.width, sizes.height)
       renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+      if (cassettePlayerX) {
+         const scaleFactor = baseHeight / sizes.height  * 8
+         cassettePlayerX.scale.set(scaleFactor, scaleFactor, scaleFactor)
+
+         console.log(scaleFactor)
+      }
    })
 
 /* 
@@ -98,18 +137,24 @@ Objects
       cassettePlayerX.add(cassettePlayerY)
       cassettePlayerY.add(gltf.scene)
 
-      gltf.scene.scale.set(10, 10, 10)
+      if (cassettePlayerX) {
+         const scaleFactor = baseHeight / sizes.height * 8
+         cassettePlayerX.scale.set(scaleFactor, scaleFactor, scaleFactor)
+      }
       cassettePlayerX.rotation.set(-0.7, 0, 0)
-      gltf.scene.position.set(0, -1, 0)
+      gltf.scene.position.set(0, 0, 0)
 
       cassettePlayerX.position.set(0, -5, 0)
       scene.add(cassettePlayerX)
+
+      window.scrollTo(0, 0)
+      ScrollTrigger.refresh()
 
       gsap.fromTo(
          cassettePlayerX.position, 
          { y: -5 },
          {
-            y: 0,        // финальная позиция
+            y: -1.2,        // финальная позиция
             duration: 1, // 3 секунды
             ease: "power2.out",
             delay: 0,   // подождать пока чёрный экран исчезнет
@@ -122,19 +167,57 @@ Objects
 
       const timeLine = gsap.timeline({
          scrollTrigger: {
-            trigger: '.container',
+            trigger: '#timeLine1',
             start: 'top top',
             end: 'bottom bottom',
             scrub: true,
             markers: true
          }
-      })
-      timeLine.to(cassettePlayerX.position, { y: -2.3, z: 1 })
-              .to(cassettePlayerX.position, { x: 1, y: '-=3', z: 0})
-              .to(cassettePlayerX.rotation, { x: '+=0.7' }, '<')
-              .to(cassettePlayerY.rotation, { y: -0.5 }, '<')
-              .to(cassettePlayerX, { autoAlpha: 0 }, '<')
+      })//.fromTo(cassettePlayerX.position, 
+         //{ y: -1.2, z: 0 },
+         //{ y: -1.2, z: 1})
+        .to(cassettePlayerX.position, { y: '-=2.1', z: '+=1' })
+        .to(cassettePlayerX.position, { x: 1, y: '-=3.4', z: 0 })
+        .to(cassettePlayerX.rotation, { x: '+=0.7' }, '<')
+        .to(cassettePlayerY.rotation, { y: -0.5 }, '<')
 
+      ScrollTrigger.create({
+         trigger: "#warmth",
+         start: 'bottom center'
+      })
+
+      ScrollTrigger.create({
+         trigger: '#cards',
+         start: 'top center',
+         onEnter: function() {
+            if (cassettePlayerX) {
+               gsap.set(cassettePlayerX.position, { x: -2, y: -16.8, z: 1})
+               gsap.set(cassettePlayerY.rotation, { x: 0, y: 0.5, z: 0})
+               gsap.set(cassettePlayerX.rotation, { x: 0, y: 0, z: Math.PI / 2}, '<')
+            }
+         },
+         onLeaveBack: function() {
+            if (cassettePlayerX) {
+               // Вернуть в исходное положение и поворот
+               gsap.set(cassettePlayerX.position, { x: 1, y: -6.6, z: 0 }) // значения подберите под вашу сцену
+               gsap.set(cassettePlayerY.rotation, { y: -0.5 })
+               gsap.set(cassettePlayerX.rotation, { x: 0, y: 0, z: 0 })
+            }
+         }
+      })
+
+      const timeLine2 = gsap.timeline({
+         scrollTrigger: {
+            trigger: '#timeLine2',
+            start: 'top top',
+            end: 'bottom bottom',
+            scrub: true,
+            markers: true
+         }
+      }).to(cassettePlayerX.position, { x: 4 })
+        .to(cassettePlayerX.position, { y: '-=6'})
+        .to(cassettePlayerX.rotation, { z: -Math.PI / 2}, '<')
+        .to(cassettePlayerX.position, { x: 2})
       
    })
 
@@ -177,11 +260,11 @@ Objects
 /* 
 Light 
 */
-   const directionalLight = new THREE.DirectionalLight(0xffffff, 2.5)
+   const directionalLight = new THREE.DirectionalLight(0xffffff, 5)
    directionalLight.position.set(1, 3, 2)
    scene.add(directionalLight)
 
-   const ambientLight = new THREE.AmbientLight(0xffffff, 5)
+   const ambientLight = new THREE.AmbientLight(0xffffff, 7)
    scene.add(ambientLight)
 
 /* 
